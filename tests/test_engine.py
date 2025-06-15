92724c660ceac3cbb56a092311a61cfe579498ae
arobopsychologist <edwardjlsh@pm.me>
2025-06-15 15:13:00 +0200

    Add value object and initial operators

---
+++ tests/test_engine.py
@@ -0,0 +1,15 @@
+from micrograd.engine import Value
+
+def test_value_add():
+    a = Value(2.0)
+    b = Value(-3.0)
+    expected_sum = Value(-1.0)
+
+    assert a + b == expected_sum
+
+def test_value_mult():
+    a = Value(2.0)
+    b = Value(-3.0)
+    expected_product = Value(-6.0)
+
+    assert a * b == expected_product
