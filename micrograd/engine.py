9087684475c0f9c3aaf444f0586e1b1c1bb43d82
arobopsychologist <edwardjlsh@pm.me>
2025-06-15 15:52:07 +0200

    add run tests on save

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,7 +1,8 @@
 class Value:
 
-    def __init__(self, data):
+    def __init__(self, data, _children=()):
         self.data = data
+        self._prev = set(_children)
     
     def __repr__(self):
         return f"Value(data={self.data})"
