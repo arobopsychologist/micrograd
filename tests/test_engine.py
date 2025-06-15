e22219d68e6d9f325f4b0769354b4eba005339ad
arobopsychologist <edwardjlsh@pm.me>
2025-06-15 15:28:48 +0200

    Add tests for quality and not equality

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,17 @@
 from micrograd.engine import Value
 
+def test_value_eq():
+    a = Value(2.0)
+    b = Value(2.0)
+
+    assert a == b
+
+def test_value_neq():
+    a = Value(2.0)
+    b = Value(3.0)
+
+    assert a != b
+
 def test_value_add():
     a = Value(2.0)
     b = Value(-3.0)
