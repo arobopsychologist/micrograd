a773a747be2768a26aac2a23d4ab35b0f51c653d
arobopsychologist <edwardjlsh@pm.me>
2025-06-15 16:48:08 +0200

    Add linting

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,17 +1,22 @@
 class Value:
 
-    def __init__(self, data, _children=()):
+    def __init__(self, data, _children=(), _op=""):
         self.data = data
+        self.grad = 0.0
         self._prev = set(_children)
-    
+        self._op = _op
+
     def __repr__(self):
         return f"Value(data={self.data})"
-    
+
     def __add__(self, other):
-        return Value(self.data + other.data)
+        return Value(self.data + other.data, (self, other), "+")
 
     def __mul__(self, other):
-        return Value(self.data * other.data)
+        return Value(self.data * other.data, (self, other), "*")
 
     def __eq__(self, other):
         return self.data == other.data
+
+    def __hash__(self):
+        return hash(self.data)
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,10 +1,12 @@
 from micrograd.engine import Value
 
+
 def test_value_eq():
     a = Value(2.0)
     b = Value(2.0)
 
     assert a == b
+
 
 def test_value_neq():
     a = Value(2.0)
@@ -12,19 +14,22 @@ def test_value_neq():
 
     assert a != b
 
+
 def test_value_add():
     a = Value(2.0)
     b = Value(-3.0)
     expected_sum = Value(-1.0)
 
-    assert a + b == expected_sum
+    assert expected_sum == a + b
+
 
 def test_value_mult():
     a = Value(2.0)
     b = Value(-3.0)
     expected_product = Value(-6.0)
 
-    assert a * b == expected_product
+    assert expected_product == a * b
+
 
 def test_value_add_associative():
     a = Value(1.0)
@@ -33,11 +38,13 @@ def test_value_add_associative():
 
     assert (a + b) + c == a + (b + c)
 
+
 def test_value_add_commutative():
     a = Value(1.0)
     b = Value(2.0)
 
     assert a + b == b + a
+
 
 def test_value_mult_associative():
     a = Value(1.0)
@@ -46,11 +53,13 @@ def test_value_mult_associative():
 
     assert (a * b) * c == a * (b * c)
 
+
 def test_value_mult_commutative():
     a = Value(1.0)
     b = Value(2.0)
 
     assert a * b == b * a
+
 
 def test_value_complex_expression():
     a = Value(1.0)
@@ -59,4 +68,28 @@ def test_value_complex_expression():
     expected_value = Value(36.0)
 
     print(a + b * c)
-    assert a + b * c == expected_value
+    assert expected_value == a + b * c
+
+
+def test_value_backwards_traversal():
+    a = Value(2.0)
+    b = Value(3.0)
+    c = Value(10.0)
+    expected_value = {a * b, c}
+
+    d = a * b + c
+    d_prev = d._prev
+
+    assert expected_value == d_prev
+
+
+def test_value_op():
+    a = Value(2.0)
+    b = Value(3.0)
+    c = Value(10.0)
+    expected_value = "+"
+
+    d = a * b + c
+    d_op = d._op
+
+    assert expected_value == d_op
